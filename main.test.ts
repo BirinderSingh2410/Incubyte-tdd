4214d12ceb391b1241c99a01eaa66750ef93ecf0
Birinder Singh <birindersingh2410@gmail.com>
2025-03-23 12:35:07 +0530

    Test case for numbers

--- main.test.ts
+++ main.test.ts
@@ -4,3 +4,14 @@ test('returns 0 if there is an empty string', () => {
     expect(stringCalculator('')).toBe(0);
 });
 
+test('returns number if only number is passed', () => {
+    expect(stringCalculator('5')).toBe(5);
+});
+
+test('returns sum of the string', () => {
+    expect(stringCalculator('1,2')).toBe(3);
+    expect(stringCalculator('6,7')).toBe(13);
+    expect(stringCalculator('1,1,2,2,4')).toBe(10);
+    expect(stringCalculator('50,50,100,80')).toBe(280);
+});
+
--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
 export const stringCalculator = (numbers: string): number => {
-    if(numbers === ''){
-        console.log("This is empty string");
-    };
+  if (numbers === "") {
+    console.log("This is empty string");
     return 0;
+  } else {
+    const numberArray = numbers.split(",").map((num) => parseInt(num));
+    const sum = addNumbers(numberArray);
+    console.log("This is sum:", sum)
+    return sum;
+  }
+};
+
+const addNumbers = (numberArray: number[]) => {
+  let sum = 0;
+  if (numberArray.length > 0) {
+    numberArray.map((num) => {
+      sum = sum + num;
+    });
+    return sum
+  }else{
+    return numberArray[0]
+  }
 };
